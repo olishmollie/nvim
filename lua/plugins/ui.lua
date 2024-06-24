234be62629d378c81167456471f062e51a7c3159
Andrew Bond <olishmollie@gmail.com>
2024-06-23 19:24:52 -0600

    Use gruvbox-material for lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -54,6 +54,9 @@ return {
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            theme = "gruvbox-material"
+        },
         config = true
     }
 }
