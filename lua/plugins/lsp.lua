0b4b47c1341f1fffd28e86a265a6350085b3ed12
Andrew Bond <olishmollie@gmail.com>
2024-12-14 14:49:08 -0700

    Add vimtex

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -55,5 +55,13 @@ return {
                 cmp_autopairs.on_confirm_done()
             )
         end
+    },
+    {
+        "lervag/vimtex",
+        lazy = false, -- we don't want to lazy load VimTeX
+        init = function()
+            -- VimTeX configuration goes here, e.g.
+            vim.g.vimtex_view_method = "zathura"
+        end
     }
 }
