467464be40349db886dcb0d282da83045f88ca52
Andy Bond <abond@emporiaenergy.com>
2025-03-14 09:31:20 -0600

    Formatting fix

--- init.lua
+++ init.lua
@@ -20,19 +20,19 @@ vim.api.nvim_set_option("clipboard", "unnamedplus")
 -- Bootstrap plugin manager.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  change_detection = { enabled = false }
+    change_detection = { enabled = false }
 })
 require("config")
 
